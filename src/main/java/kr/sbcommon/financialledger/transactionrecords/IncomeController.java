447c04e3ded8d10666453e8c5aa93a7a77adaa01
손정운 <jungwoon571@gmail.com>
2024-03-07 09:52:35 +0900

    chore: TransactionRecordController.java -> IncomeController.java 수정

--- src/main/java/kr/sbcommon/financialledger/transactionrecords/IncomeController.java
+++ src/main/java/kr/sbcommon/financialledger/transactionrecords/IncomeController.java
@@ -7,58 +7,59 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-public class TransactionRecordController {
+@RequestMapping("/income")
+public class IncomeController {
 
     private final TransactionRecordService transactionRecordService;
 
-    public TransactionRecordController(TransactionRecordService transactionRecordService) {
+    public IncomeController(TransactionRecordService transactionRecordService) {
         this.transactionRecordService = transactionRecordService;
     }
 
-    @RequestMapping("/income")
+    @GetMapping("/")
     public String income() {
         return "transactionrecords/income";
     }
 
-    @RequestMapping("/income/{year}/{month}")
+    @GetMapping("/{year}/{month}")
     public ResponseEntity<List<TransactionRecordDto>> list(@PathVariable int year, @PathVariable int month) {
         return ResponseEntity.ok(transactionRecordService.findIncomeRecords(year, month));
     }
 
-    @PostMapping("/income")
+    @PostMapping("/")
     public ResponseEntity<Long> createIncome(@RequestBody CreateRecordRequestDto dto) {
         return ResponseEntity.ok(transactionRecordService.findNewIncomeRecordId(dto));
     }
 
-    @DeleteMapping("/income/{id}")
-    public ResponseEntity<Long> deleteIncome(@PathVariable Long id) {
-        transactionRecordService.delete(id);
-        return ResponseEntity.noContent()
-                .build();
-    }
-
-    @PutMapping("/income/category")
+    @PutMapping("/category")
     public ResponseEntity<TransactionRecordDto> updateCategory(@RequestBody UpdateRecordRequestDto dto) {
         return ResponseEntity.ok(transactionRecordService.updateCategory(dto));
     }
 
-    @PutMapping("/income/name")
+    @PutMapping("/name")
     public ResponseEntity<TransactionRecordDto> updateName(@RequestBody UpdateRecordRequestDto dto) {
         return ResponseEntity.ok(transactionRecordService.updatePersonName(dto));
     }
 
-    @PutMapping("/income/amount")
+    @PutMapping("/amount")
     public ResponseEntity<TransactionRecordDto> updateAmount(@RequestBody UpdateRecordRequestDto dto) {
         return ResponseEntity.ok(transactionRecordService.updateAmount(dto));
     }
 
-    @PutMapping("/income/details")
+    @PutMapping("/details")
     public ResponseEntity<TransactionRecordDto> updateDetails(@RequestBody UpdateRecordRequestDto dto) {
         return ResponseEntity.ok(transactionRecordService.updateDetails(dto));
     }
 
-    @PutMapping("/income/date")
+    @PutMapping("/date")
     public ResponseEntity<TransactionRecordDto> updateDate(@RequestBody UpdateRecordRequestDto dto) {
         return ResponseEntity.ok(transactionRecordService.updateDate(dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> deleteIncome(@PathVariable Long id) {
+        transactionRecordService.delete(id);
+        return ResponseEntity.noContent()
+                .build();
     }
 }
